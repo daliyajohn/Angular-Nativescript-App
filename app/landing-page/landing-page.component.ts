4e6ef7986e3a799ef292a727ca0dcc292447608a
daliyajohn <daliyajohn75@gmail.com>
2018-05-27 20:49:47 +0530

    UI  fix

--- app/landing-page/landing-page.component.ts
+++ app/landing-page/landing-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ns-landing',
   moduleId: module.id,
-  templateUrl: './landing-page.component.html',
+  templateUrl: 'landing-page.component.html',
   styleUrls: ['landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
