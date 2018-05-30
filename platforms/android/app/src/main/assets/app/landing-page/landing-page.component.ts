465031982dea9bbf4444de783ac94a0cd858961b
daliyajohn <daliyajohn75@gmail.com>
2018-05-30 15:04:52 +0530

    Bug fix

--- platforms/android/app/src/main/assets/app/landing-page/landing-page.component.ts
+++ platforms/android/app/src/main/assets/app/landing-page/landing-page.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ns-landing',
   moduleId: module.id,
-  templateUrl: './landing-page.component.html'
-  // styleUrls: ['landing-page.component.css']
+  templateUrl: 'landing-page.component.html',
+  styleUrls: ['landing.component.css']
 })
 export class LandingPageComponent implements OnInit {
   ngOnInit() {}
