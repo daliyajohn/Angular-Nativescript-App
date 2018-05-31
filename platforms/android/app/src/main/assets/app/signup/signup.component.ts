a0b3fa0c9d1112db6a5c932da90ccbf3fc23e681
daliyajohn <daliyajohn75@gmail.com>
2018-05-31 11:02:11 +0530

    Add signup component

---
+++ platforms/android/app/src/main/assets/app/signup/signup.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-signup',
+  moduleId: module.id,
+  templateUrl: 'signup.component.html',
+  styleUrls: ['signup.component.css']
+})
+export class SignUpPageComponent implements OnInit {
+  ngOnInit() {}
+}
