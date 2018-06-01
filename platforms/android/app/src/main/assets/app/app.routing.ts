83555da37a3079213f1844f80f066c2096e8af99
daliyajohn <daliyajohn75@gmail.com>
2018-06-01 11:37:32 +0530

    Signup , login router fix

--- platforms/android/app/src/main/assets/app/app.routing.ts
+++ platforms/android/app/src/main/assets/app/app.routing.ts
@@ -5,12 +5,16 @@ import { Routes } from '@angular/router';
 import { ItemsComponent } from './item/items.component';
 import { ItemDetailComponent } from './item/item-detail.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SignUpPageComponent } from '~/signup/signup.component';
+import { LoginPageComponent } from '~/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: LandingPageComponent },
   { path: 'items', component: ItemsComponent },
-  { path: 'item/:id', component: ItemDetailComponent }
+  { path: 'item/:id', component: ItemDetailComponent },
+  { path: 'signup', component: SignUpPageComponent },
+  { path: 'login', component: LoginPageComponent }
 ];
 
 @NgModule({
