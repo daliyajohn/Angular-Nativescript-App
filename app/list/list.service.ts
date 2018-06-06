668ff900e555a31cba473385e755eeae6ec27685
daliyajohn <daliyajohn75@gmail.com>
2018-06-06 23:48:22 +0530

    Add service and interface

---
+++ app/list/list.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ListItem } from '~/list/list.interface';
+
+@Injectable()
+export class ItemService {
+  private items = new Array<ListItem>(
+    { id: 1, name: 'Ter Stegen' },
+    { id: 3, name: 'Piqué' },
+    { id: 4, name: 'I. Rakitic' },
+    { id: 5, name: 'Sergio' },
+    { id: 6, name: 'Denis Suárez' },
+    { id: 7, name: 'Arda' },
+    { id: 8, name: 'A. Iniesta' },
+    { id: 9, name: 'Suárez' }
+  );
+
+  getLists(): ListItem[] {
+    return this.items;
+  }
+
+  getItem(id: number): ListItem {
+    return this.items.filter(item => item.id === id)[0];
+  }
+}
